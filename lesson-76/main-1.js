04ec8d10d1b90c10bb36b426eaa579f2eb641750
stubblymainstay <164861751+stubblymainstay@users.noreply.github.com>
2024-08-09 17:50:36 +1000

    add

---
+++ lesson-76/main-1.js
@@ -0,0 +1,28 @@
+const Name = `Никита`; 
+const age = 17; 
+const city = `Комсомольск-на-амуре`; 
+const profession = `Программист`; 
+const hobbies = `игры`; 
+
+console.log (`Имя:`,Name);
+console.log (`Возраст:`,age);
+console.log (`Город:`,city);
+console.log (`Профессия:`,profession);
+console.log (`Хобби:`,hobbies);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
