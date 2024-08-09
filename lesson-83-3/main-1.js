e14b048d50a81d356f9b4c9cb28128dc26df79a7
stubblymainstay <164861751+stubblymainstay@users.noreply.github.com>
2024-08-09 15:29:51 +1000

    Add files via upload

---
+++ lesson-83-3/main-1.js
@@ -0,0 +1,43 @@
+const users = [
+  { name: "Ваня", 
+    age: 28, 
+  isAdmin: false },
+  { name: "Саша", 
+    age: 25, 
+  isAdmin: true },
+  { name: "Настя", 
+    age: 20, isAdmin: 
+    false },
+  { name: "Лёша", 
+    age: 19, 
+  isAdmin: false },
+  { name: "Стас", 
+    age: 32, 
+    isAdmin: true }
+];
+
+let NumberOfAdminst = 0;
+
+for (let i = 0; i < users.length; i++) {
+  if (!users[i].isAdmin) {
+    NumberOfAdminst++; }
+    else { continue;
+      
+    }
+}
+
+console.log(`Всего`, NumberOfAdminst ,`простых пользователей`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson-83/main-1.js
@@ -0,0 +1,27 @@
+const ItsMe = {
+name: `Никита`,
+age: 17,
+isAdmin: false, 
+sayHello() {
+  console.log(`Hello ${this.name}`);
+  }
+};
+
+ItsMe.sayHello();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
