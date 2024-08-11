003e98d99825978f3672a62f13c62c4590b598ec
stubblymainstay <164861751+stubblymainstay@users.noreply.github.com>
2024-08-11 18:26:29 +1000

    Add files via upload

---
+++ lesson-92-2/main.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector(`.btn-open`)
+const modal = document.querySelector(`.modal`)
+const body = document.body
+
+const classModal = () => {
+    modal.classList.remove(`modal--open`) 
+    body.classList.remove(`body--fixed`)
+}
+const openModal = () => {
+    modal.classList.add(`modal--open`)
+    body.classList.add(`body--fixed`)
+}
+
+
+btn.addEventListener(`click`, openModal)
+
+modal.addEventListener(`click`, event => {
+    const target = event.target
+    if (target && target.classList.contains(`modal`) || target.classList.contains(`modal__close-btn`)) {
+        classModal() 
+    }
+    }
+)
+
+document.addEventListener(`keydown`, event =>  {
+    if (event.code === 'Escape' && modal.classList.contains(`modal--open`)) {
+        classModal()
+
+    }
+}
+)
+
+
+
